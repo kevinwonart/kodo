456fdf282d202c86235858a6164a39a8198338e3
Kevin Won <kevin.won91@gmail.com>
2023-06-30 11:07:51 -0500

    Initial commit

---
+++ kodo/tasks/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        return redirect('task_list')
+    return render(request, 'tasks/task_create.html')
+
